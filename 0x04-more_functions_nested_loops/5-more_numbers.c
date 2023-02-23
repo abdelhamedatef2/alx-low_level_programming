4a3cdc266b9538276622ef3d0f3b7967283be228
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-22 20:48:59 -0800

    solve p5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#iclude "main.h"
+/*a function that prints 10 times the numbers
+ * , from 0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i = 0 , j = 0;
+
+	for (i ; i < 11 ; i++)
+	{
+		for (j ; j < 15 ; j++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
