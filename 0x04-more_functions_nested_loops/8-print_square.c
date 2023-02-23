0c96f7ad338d96bd46f04c3e4fd7e10e365662e5
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-23 09:24:40 -0800

    fix code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,17 +12,18 @@ void print_square(int size)
 {
 	int row, column;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	else
-	{	
-		for (row = 1; row <= size; ++row)
+		if (size <= 0)
 		{
-			for (column = 1; column <= size; ++column)
-				_putchar('#');
 			_putchar('\n');
 		}
-	}
-	}
+		else
+		{	
+			for (row = 1; row <= size; ++row)
+			{
+				for (column = 1; column <= size; ++column)
+					_putchar('#');
+				_putchar('\n');
+			}
+		}
+		
 }
