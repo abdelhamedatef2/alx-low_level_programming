d2f9e2ab1a746911c512552ff290d5689ed90ab0
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-23 10:03:15 -0800

    fix code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,69 +1,23 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * _sqrt - finds the square root
- *
- * @x: input number
- *
- * Return: square root of x
- *
+* print_number - prints an integer.
+*@n: is the integer to be printed.
 */
 
-double _sqrt(double x)
+void print_number(int n)
 {
-	float sqrt, tmp;
 
-	sqrt = x / 2;
-	tmp = 0;
+unsigned int a = n;
 
-	while (sqrt != tmp)
+	if (n < 0)
 	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-	return (sqrt);
-}
-
-/**
- * largest_prime_factor - finds and prints the largest
- *			prime factor of number (num)
- *
- * @num: number to find
-*/
-
-void largest_prime_factor(long int num)
-{
-	int prmNu, largest;
-
-	/* first divide with the smallest prime number (two) */
-	while (num % 2 == 0)
-		num = num / 2;
-
-	/* num must be odd so we proceed to the next prime number (plus two) */
-	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-	{
-		while (num % prmNu == 0)
-		{
-			num = num / prmNu;
-			largest = prmNu;
-		}
+		_putchar('-');
+		a = -a;
 	}
 
+	else if ((a / 10) > 0)
+		print_number(a / 10);
 
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
-int main(void)
-{
-
-	largest_prime_factor(612852475143);
-
-	return (0);
+	_putchar((a % 10) + '0');
 }
