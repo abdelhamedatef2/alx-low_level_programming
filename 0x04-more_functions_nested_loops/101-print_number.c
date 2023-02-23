a2426934fd8e3d434d0f8ca081197b6a5878f8a6
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-23 10:20:44 -0800

    solve more and more problem

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,14 @@
 void printnumber(int n)
 {
 
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
-
-    if (n == 0)
-        putchar('0');
-
-    if (n/10)
-        printnumber(n/10);
-
-    putchar(n%10 + '0');
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		printnumber(n / 10);
+	_putchar(n % 10 + '0');
 }
