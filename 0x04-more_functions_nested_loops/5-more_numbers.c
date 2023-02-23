432f7b43511de50fd21edbb005f210dc30df35a6
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-23 09:04:37 -0800

    fix code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * more_numbers - print 0 - 14 ten times and you
@@ -9,15 +9,19 @@
 
 void more_numbers(void)
 {
-	int i = 0, j = 0;
+	int num, row, count;
 
-	for (i ; i < 11 ; i++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (j ; j < 15 ; j++)
+		for (count = 0; count <= 14; ++count)
 		{
-			if (b > 9)
-				_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
