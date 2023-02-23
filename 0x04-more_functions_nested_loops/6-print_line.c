59ccc1860b02dfd7fdeb58975f9cea1e9991efe9
abdelhamedatef2 <abdelhamedatef2@gmail.com>
2023-02-23 09:12:32 -0800

    fix code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_line - print a straight line
@@ -11,12 +11,12 @@ void print_line(int n)
 {
 	int line;
 
-	if (n > 0)
-	{
-		for (line = 1; line < n; line++)
-			_putchar('_');
-		_putchar('_');
-	}
+	if (n <= 0)
+		_putchar('\n');
 	else
-	_putchar('\n');
+	{
+		for (line = 1; line <= n; line++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
