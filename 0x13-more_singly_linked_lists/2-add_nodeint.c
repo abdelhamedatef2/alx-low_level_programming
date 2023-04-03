5ee79ca3dd57456c5a85854c11b1e5e8303c7b08
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-03 08:36:13 -0700

    fix betty's errors

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * add_nodeit - adds a new node at the beginnning
- * of a list
- * @head: pointer to the first element
- * @n: int to be copied
- * description: a function that adds a new node at the
- * beginning of a listint_t list.
- * return: new node
+ * add_nodeint - Adds a new node at the beginning
+ *               of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
