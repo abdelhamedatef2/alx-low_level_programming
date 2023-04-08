4ecfd3c10caec54cb0e3856b934e96661000b0a9
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-08 11:47:34 -0700

    fix code errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,20 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number = 0;
-	int length = 0;
+	unsigned int num = 0;
+	int x = 0;
 
-	if (b[length] == '\0')
+	if (b[x] == '\0')
 		return (0);
 
-	while ((b[[length] == '0') || (b[length] == '1'))
+	while ((b[x] == '0') || (b[x] == '1'))
 	{
-		number <<= 1;
-		num += b[length] - '0';
-		length++;
+		num <<= 1;
+		num += b[x] - '0';
+		x++;
 	}
 
-	return (number);
+	return (num);
 }
