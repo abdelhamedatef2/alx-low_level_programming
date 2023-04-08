f9379f493c64d85709d6615734a763e5fab17291
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-08 11:48:51 -0700

    fix code errors again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int x = 0;
+	int len = 0;
 
-	if (b[x] == '\0')
+	if (b[len] == '\0')
 		return (0);
 
-	while ((b[x] == '0') || (b[x] == '1'))
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
 		num <<= 1;
-		num += b[x] - '0';
-		x++;
+		num += b[len] - '0';
+		len++;
 	}
 
 	return (num);
