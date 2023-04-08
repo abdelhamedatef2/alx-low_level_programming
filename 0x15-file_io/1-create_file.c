101eeb4fcbec0a23ee469b849091268bced45540
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-08 13:24:08 -0700

    fix bettty errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		for (x = 0; text_content[x];)
 			x++;
 	}
-	
+
 	w = write(o, text_content, x);
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
