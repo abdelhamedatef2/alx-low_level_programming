93fa46117a1d7b3283c8da30dab22a9b277f4cae
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-04-09 17:22:10 -0700

    solve p2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - return value of index
+ * @n: unsigned long int to search
+ * @index: index num to return
+ * Return: the value of the bit at index index or -1
+ *  if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 53)
+		return (-1);
+	return ((n>> index) & 1);
+}
