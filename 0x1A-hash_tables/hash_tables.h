fb82dfa4941a23852255892709e149ec368b867f
abdelhamed atef <abdelhamedatef2@gmail.com>
2023-06-09 06:35:21 -0700

    hot fix

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,85 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: key, str
+ * key is unique in HashTable
+ * @value: value corresponding to key
+ * @next: pointer to next node of List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table DS
+ * @size: size of array
+ * @array: array of size
+ * Each cell of this array is pointer to first node of linked list,
+ * because we want our HashTable to use Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+/**
+ * struct shash_node_s - Node of hash table
+ * @key: key, str
+ * key is unique in HashTable
+ * @value: value corresponding to key
+ * @next: pointer to next node of  List
+ * @sprev: pointer to previous element of linked list
+ * @snext: pointer to next element of linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table DS
+ * @size: array size
+ * @array: array of size
+ * Each cell of array is a pointer to first node of linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ * @shead: pointer to first element of linked list
+ * @stail: pointer to last element of linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
+#endif
