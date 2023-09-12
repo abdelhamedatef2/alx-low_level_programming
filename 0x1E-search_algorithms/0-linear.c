cf6f1c94f34b23e4d935bf4c8efcded59f27bf93
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-09-12 14:45:46 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,13 +12,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i = 0;
 
-	for (i = 0; array && i < size; ++i)
+	if (array == NULL)
+		return (-1);
+
+	for (; i < (int)size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
 	}
+
 	return (-1);
 }
